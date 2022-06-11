0c878a7881012696ca8a7dc691f207f12b83a0c4
suraj1947 <suraj1947>
2022-06-11 12:37:31 +0000

    Initial commit

---
+++ src/test/java/com/christmas/ChristmasLightsTest.java
@@ -0,0 +1,21 @@
+package com.christmas;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class ChristmasLightsTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
